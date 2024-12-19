7152e7f9d95eb6724736a8f0227f7c7763b0edf2
L <luke.pialloux28@student.tamdistrict.org>
2024-12-19 11:20:08 -0800

    game

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-let maxvar= "max"
-let myvar = "hi"+" "+maxvar
-console.log(myvar)
+
+let characterEl = document.getElementById("character")
+
+document.addEventListener("keydown", (event) => {
+    if(event.key == "w"){
+        let previousTop = characterEl.style.top;
+        characterEl.style.top = Number(previousTop.substring(0,previousTop.length-2)) - 2 + "px"
+    }
+})
+
+document.addEventListener("keydown", (event) => {
+    if(event.key == "s"){
+        let previousTop = characterEl.style.top;
+        characterEl.style.top = Number(previousTop.substring(0,previousTop.length-2)) + 2 + "px"
+    }
+})
+
+document.addEventListener("keydown", (event) => {
+    if(event.key == "a"){
+        let previousTop = characterEl.style.left;
+        characterEl.style.left = Number(previousTop.substring(0,previousTop.length-2)) - 2 + "px"
+    }
+})
+
+document.addEventListener("keydown", (event) => {
+    if(event.key == "d"){
+        let previousTop = characterEl.style.left;
+        characterEl.style.left = Number(previousTop.substring(0,previousTop.length-2)) + 2 + "px"
+    }
+})
